d69b97ab83a9f89666722dcb7814216bffd2a7d4
enzosamojedny <enzosamojedny@gmail.com>
2023-10-11 08:49:54 -0300

    fix: searchbar fixed
    There was a bug where searchbar would be permanently enabled

--- client/src/components/navbar/SearchBar.jsx
+++ client/src/components/navbar/SearchBar.jsx
@@ -1,47 +1,57 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, useParams } from 'react-router-dom'
-import { useState } from 'react'
-import { getVideogameName } from '../../redux/actions.js'
-import './searchbar.css'
+import React, { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useParams } from 'react-router-dom';
+import { getVideogameName } from '../../redux/actions.js';
+import './searchbar.css';
 
 function SearchBar() {
-    const { name } = useParams()
-    const dispatch = useDispatch()
-    const { videogameName } = useSelector((state) => state)
+    const { name } = useParams();
+    const dispatch = useDispatch();
+    const { videogameName } = useSelector((state) => state);
     const [searchInput, setSearchInput] = useState(name || '');
     const [inputFocused, setInputFocused] = useState(false);
+    const searchContainer = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (searchContainer.current && !searchContainer.current.contains(event.target)) {
+            enableBodyScroll();
+            setInputFocused(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     const handleClick = (event) => {
-        event.preventDefault()
-        dispatch(getVideogameName(searchInput))
-    }
+        event.preventDefault();
+        dispatch(getVideogameName(searchInput));
+    };
+
     const handleSearchFocus = () => {
         setInputFocused(true);
     };
 
-    const handleSearchBlur = () => {
-        enableBodyScroll();
-        setInputFocused(false);
-    };
-
-
-    const disableBodyScroll = () => {
-        document.body.style.overflow = 'hidden';
-    };
 
     const enableBodyScroll = () => {
         document.body.style.overflow = 'auto';
+        document.documentElement.style.overflow = 'auto';
     };
+
     function shouldHideSearchBar() {
         if (!videogameName) {
-            return searchContent.length === 0;
+            return searchInput.length === 0;
         }
         return false;
     }
+
     return (
-        <div>
+        <div ref={searchContainer}>
             <form className='searchbar-container'>
-                <input type="search" className='navbar-searchbar' placeholder='Browse...' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onFocus={handleSearchFocus} onBlur={handleSearchBlur} />
+                <input type="search" className='navbar-searchbar' placeholder='Browse...' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} onFocus={handleSearchFocus} />
                 <button className='bn3' onClick={handleClick} type='submit'>Search</button>
             </form>
             <div className='navbar-wrapper'>
@@ -59,13 +69,11 @@ function SearchBar() {
                                 </NavLink>
                             </div>
                         ))}
-
                     </div>
                 )}
             </div>
-
         </div>
-    )
+    );
 }
 
 export default SearchBar;
